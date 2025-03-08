bd0c3f40e6054bfd73ce99fc856cd5b2f3067a2d
teksite <teksite.design@gmail.com>
2025-03-08 10:48:02 +0330

    refactor migare and reset command (fix bug)

--- src/Console/Migrate/MigrateCommands.php
+++ src/Console/Migrate/MigrateCommands.php
@@ -7,85 +7,31 @@ use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
+use Lareon\Modules\Page\Contract\MigrationContract;
+use Lareon\Modules\Page\Services\BasicMigrator;
 use Symfony\Component\Console\Input\InputOption;
 use Teksite\Module\Facade\Module;
 use Teksite\Module\Traits\ModuleCommandsTrait;
 use Teksite\Module\Traits\ModuleNameValidator;
 
-class MigrateCommands extends Command
+class MigrateCommands extends  BasicMigrator implements MigrationContract
 {
     use ModuleNameValidator, ModuleCommandsTrait;
 
-    protected $signature = 'module:migrate {module?} {--database=} {--force} {--realpath} {--pretend} {--seed} {--step} {--isolated}';
+    protected $signature = 'module:migrate {module?}  {--seed}';
 
     protected $description = 'Run the migrations for a specific module or all modules';
 
     public function handle()
     {
-        $module = $this->argument('module');
+       parent::handle();
+       if ($this->option('seed')) $this->seeding();
 
-        if ($module) {
-            $this->runMigrationsForModule($module);
-        } else {
-            $this->runMigrationsForAllModules();
-        }
     }
 
-    protected function runMigrationsForModule($module)
+    public function runTheCommand($module)
     {
-
-        [$isValid, $suggestedName] = $this->validateModuleName($module);
-        if ($isValid) {
-            $this->runMigrateCommand($module);
-            return;
-        }
-
-        if ($suggestedName && $this->confirm("Did you mean '{$suggestedName}'?")) {
-            $this->runMigrateCommand($suggestedName);
-            return;
-        }
-        $this->error("The module '" . $module . "' does not exist.");
-        return 1;
+        $this->up($module);
     }
 
-
-    protected function runMigrationsForAllModules()
-    {
-        foreach (Module::all() as $module) {
-            $this->info("Running migrations for module: " . $module);
-            $this->runMigrationsForModule($module);
-        }
-    }
-
-    protected function runMigrateCommand($module)
-    {
-
-        $options = [
-            '--database' => $this->option('database'),
-            '--force' => $this->option('force'),
-            '--realpath' => $this->option('realpath'),
-            '--pretend' => $this->option('pretend'),
-            '--step' => $this->option('step'),
-        ];
-
-        $migrationsPath = module_path($module, 'Database/Migrations');
-        $seedersPath = module_path($module, "Database/Seeders");
-
-        $migration_list = File::allFiles($migrationsPath);
-
-        $allFiles = [];
-        foreach ($migration_list as $migrateFile) {
-            $allFiles[] = str_replace(base_path(), '', $migrateFile);
-        }
-        $options['--path'] = $allFiles;
-
-        Artisan::call('migrate', $options, $this->output);
-
-        if ($this->option('seed') && File::exists($seedersPath)) {
-            Artisan::call('module:seed', [
-                '--module' => "$module"
-            ], $this->output);
-        }
-
-    }
 }
--- src/Console/Migrate/ResetCommands.php
+++ src/Console/Migrate/ResetCommands.php
@@ -4,16 +4,21 @@ namespace Teksite\Module\Console\Migrate;
 
 use Illuminate\Console\Command;
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
+use Lareon\Modules\Page\Contract\MigrationContract;
+use Lareon\Modules\Page\Services\BasicMigrator;
 use Symfony\Component\Console\Input\InputOption;
 use Teksite\Module\Facade\Module;
 use Teksite\Module\Traits\ModuleCommandsTrait;
 use Teksite\Module\Traits\ModuleNameValidator;
 
-class ResetCommands extends Command
+class ResetCommands extends BasicMigrator implements MigrationContract
 {
     use ModuleNameValidator, ModuleCommandsTrait;
 
@@ -22,73 +27,9 @@ class ResetCommands extends Command
 
     protected $description = 'Rollback migrations for a specific module or all modules';
 
-    public function handle()
+
+    public function runTheCommand($module): void
     {
-        $modules = $this->getModules();
-         foreach ($modules as $module) {
-             $correctedModule = $this->checkModule($module);
-             if (!$correctedModule){
-                 $this->error("The module '" . $module . "' does not exist.");
-                 return 0;
-             }
-             $this->runTheCommand($correctedModule);
-
-         }
-    }
-
-    protected function checkModule($module) : false|string
-    {
-
-        [$isValid, $suggestedName] = $this->validateModuleName($module);
-        if ($isValid) {
-            return $module;
-        }
-
-        if ($suggestedName && $this->confirm("Did you mean '{$suggestedName}'?")) {
-            return $suggestedName;
-        }
-        return false;
-    }
-
-    protected function runTheCommand($module)
-    {
-        $this->info("Dropping all tables for module: " . $module);
-
-        $migrationsPath = module_path($module, 'Database/Migrations');
-
-        $migration_list = File::allFiles($migrationsPath);
-        $allFiles = [];
-        foreach ($migration_list as $migrateFile) {
-            $absPath = $migrateFile->getPathname();
-            $this->runAndCalculate(function () use ($absPath) {
-                $class=$this->resolve($absPath);
-
-            });
-        }
-
-
-    }
-
-    public function resolve($file)
-    {
-        return include $file;
-    }
-
-    public function runAndCalculate(\Closure $closer, string $first = '', string $second = ''): void
-    {
-        $startTime = Carbon::now();
-        $closer();
-        $endTime = Carbon::now();
-
-        $executionTime = $startTime->diffInMilliseconds($endTime);
-
-        $this->components->twoColumnDetail($first, "$executionTime <fg=green;options=bold>DONE</>");
-
-    }
-
-    public function getModules()
-    {
-        $module = $this->argument('module');
-        return $module ? [$module] : Module::all();
+        $this->down($module);
     }
 }
