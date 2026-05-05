6af85f9ff3b701759e91c8b7a0c8f1af8e2b015b
teksite <teksite.design@gmail.com>
2026-05-05 17:57:12 +0330

    refactor migration aker based on laravel

--- src/Console/Make/MigrationMakeCommand.php
+++ src/Console/Make/MigrationMakeCommand.php
@@ -2,69 +2,109 @@
 
 namespace Teksite\Module\Console\Make;
 
-use Illuminate\Console\GeneratorCommand;
-use Illuminate\Support\Facades\File;
-use Teksite\Module\Facade\Module;
+use Illuminate\Console\Concerns\CreatesMatchingTest;
+use Symfony\Component\Console\Input\InputOption;
+use Teksite\Module\Console\GeneratorModuleCommand;
 
 
-
-class MigrationMakeCommand extends GeneratorCommand
+class MigrationMakeCommand extends GeneratorModuleCommand
 {
+    protected string $generatorType = 'file';
 
     /**
-     * The console command signature.
+     * The console command name.
      *
      * @var string
      */
-    protected $signature = 'module:make-migration {name} {module}
-        {--create= : The table to be created }
-        {--table= : The table to migrate }
-        ';
+    protected $name = 'module:make-migration';
+
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Create a new migration file in the specific module';
+    protected $description = 'Create a new migration file in modules or steward';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected string $type = 'Migration';
 
 
-    public function handle()
+    public function handle(): void
     {
-        $module = $this->argument('module');
 
-        [$isValid, $suggestedName] = $this->validateModuleName($module);
-
-        if ($isValid) return $this->generateMigration();
-
-        if ($suggestedName && $this->confirm("Did you mean '{$suggestedName}'?")) {
-            $this->input->setArgument('module', $suggestedName);
-            return $this->generateMigration();
+        $name = $this->getNameInput();
+        if ($this->isReservedName($name)) {
+            $this->components->error('The name "' . $name . '" is reserved by PHP.');
+            return;
         }
-        $this->error("The module '".$module."' does not exist.");
-        return 1;
+        $module = $this->getModuleInput();
+        if (!$this->isModuleExist($module)) {
+            $this->components->error('The module "' . $module . ' is not registered or does not exist.');
+            $this->components->error("use steward work instead of module name to make {$this->type} in steward");
+            return;
+        }
+
+        $this->generateMigration($name, $module);
+
 
     }
-    protected function generateMigration(): void
-    {
-        $module = $this->argument('module');
-        $modulePath = Module::modulePath($module ,config('moduleconfigs.module.database.migration_path' , 'Database/Migrations'));
 
-        if (!is_dir($modulePath)){
-            File::makeDirectory($modulePath , '0755' , true);
-        }
-        $relativeBase=str_replace(base_path() , '', $modulePath);
+    protected function generateMigration(string $name, string $module): void
+    {
+
+
+        $path = $module === 'Steward'
+            ? steward_path(config('modules.steward.migration_path', 'database/migrations') , false)
+            : module_path($module, config('modules.module.migration_path', 'database/migrations') , false);
+
+        $this->ensureDirectoryExistence($path);
+
         $options = [
-            'name' => $this->argument('name'),
-            '--table' => $this->option('table') ,
+            'name'     => $this->argument('name'),
+            '--table'  => $this->option('table'),
             '--create' => $this->option('create'),
-            '--path' =>$relativeBase,
+            '--path'   => $path,
         ];
         $this->call('make:migration', $options);
     }
 
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     * @throws \Exception
+     */
 
-    protected function getStub()
+    protected function getStub(): string
     {
+        return '';
+    }
 
+    protected function path(): string
+    {
+        return 'database/migrations';
+    }
+
+    /**
+     * set replacements
+     *
+     * @return array [string $searchable , string $replace ]
+     */
+    protected function replacements(): array
+    {
+        return [];
+
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['create', 'f', InputOption::VALUE_OPTIONAL, "The table to be created"],
+            ['table', null, InputOption::VALUE_OPTIONAL, 'The table to migrate'],
+        ];
     }
 }
