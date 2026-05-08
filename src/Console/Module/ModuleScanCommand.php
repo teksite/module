1e777b1b35c1aeca240385a441ddb00a24082355
teksite <teksite.design@gmail.com>
2026-05-08 17:59:09 +0330

    restyle scaning module

--- src/Console/Module/ModuleScanCommand.php
+++ src/Console/Module/ModuleScanCommand.php
@@ -20,31 +20,37 @@ class ModuleScanCommand extends Command
 
     public function handle(): void
     {
-
+        $this->newLine();
         $widowedModules = $this->getWidowModules();
 
         if (empty($widowedModules)) {
-            $this->line("<fg=yellow>** all modules is registered before, nothing to register</>");
+            $this->line("<fg=yellow>** all modules has been registered already, nothing to scan **</>");
             $this->newLine();
             return;
         }
-
+        $widowedString = implode(',',$widowedModules);
+        $this->line("** some directories has been found: $widowedString");
+        $this->line("** now we are trying to validate and register them");
+        $this->newLine();
         foreach ($widowedModules as $module) {
+            $this->line("<fg=cyan;options=bold>{$module}</>");
 
+            $serviceProviderNameNamespace = module_namespace($module) . '\\App\\Providers\\' . $module . "ServiceProvider";
             $serviceProviderName = $module . "ServiceProvider";
-            $serviceProviderNameNamespace = module_namespace($module) . '\\Providers\\' . $module . "ServiceProvider";
 
             $serviceProviderPath = $this->getServiceProviderPath($module, $serviceProviderName);
+
             if (!$serviceProviderPath) continue;
 
             $type = $this->getModuleType($serviceProviderPath, $serviceProviderNameNamespace);
             if (!$type) continue;
 
-            $this->registerModule($module, $type , false);
+            $this->registerModule($module, $type, false);
 
-            $this->info("$module is registered");
+            $this->components->twoColumnDetail("<fg=green> └─ $module is registered</>", '<fg=green;options=bold>✓ DONE</>');
 
         }
+        $this->newLine();
         $this->dumpingComposer();
     }
 
@@ -52,7 +58,7 @@ class ModuleScanCommand extends Command
     {
         $servicePath = module_path($moduleName, "app/Providers/$serviceProviderFileName.php");
         if (!File::exists($servicePath)) {
-            $this->error("$moduleName can't be registered: $serviceProviderFileName doesn't exist");
+            $this->components->twoColumnDetail("$serviceProviderFileName.php", "<fg=red>x file can not be found</>");
             return false;
 
         }
@@ -64,7 +70,7 @@ class ModuleScanCommand extends Command
         require_once $serviceProviderPath;
 
         if (!class_exists($serviceProviderNameNamespace)) {
-            $this->error("$serviceProviderNameNamespace doesn't exist in $serviceProviderPath file");
+            $this->components->twoColumnDetail("$serviceProviderNameNamespace", "<fg=red>x class is not exists</>");
             return false;
         }
         $ref = new ReflectionClass($serviceProviderNameNamespace);
@@ -73,7 +79,8 @@ class ModuleScanCommand extends Command
 
         $type = $defaultProperties['type'] ?? false;
         if (!(bool)$type) {
-            $this->error("type property doesn't set in $serviceProviderNameNamespace, set steward or self ");
+            $this->components->twoColumnDetail("$serviceProviderNameNamespace", "<fg=red>x module type is not valid or not set</>");
+            $this->line("<fg=gray>module should be self(manged bt itself) or steward (managed by steward)</>");
             return false;
         }
         return $type;
