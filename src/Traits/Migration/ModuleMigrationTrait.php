5081a1e401be6a1b105313090d8ef8a3f259c40c
teksite <teksite.design@gmail.com>
2026-09-13 16:27:53 +0330

    refactor, fix bugs: ModuleMigrationTrait

--- src/Traits/Migration/ModuleMigrationTrait.php
+++ src/Traits/Migration/ModuleMigrationTrait.php
@@ -2,12 +2,9 @@
 
 namespace Teksite\Module\Traits\Migration;
 
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
-use Teksite\Module\Facade\Module;
 
 trait ModuleMigrationTrait
 {
@@ -17,7 +14,9 @@ trait ModuleMigrationTrait
     public function downModules(): void
     {
         $this->warn("Dropping all tables of module(s)");
+
         foreach ($this->getModules() as $mdl) {
+
             $this->runAndCalculate(function () use ($mdl) {
                 foreach (array_reverse($this->moduleMigrationFiles($mdl)) as $migration) {
                     $class = $this->resolve($migration['path']);
@@ -32,15 +31,17 @@ trait ModuleMigrationTrait
      * @param int|null $batch
      * @return void
      */
-    public function upModules(?int $batch = null): void
+    public function upModules(?int $batch = null,): void
     {
         $batch = $batch ?? DB::table('migrations')->max('batch') + 1;
 
         $migrationsRecords = $this->migrationTableRecord();
 
         foreach ($this->getModules() as $mdl) {
-            $this->warn("migrating all tables of module: " . $mdl);
+
+            $this->warn("migrating all tables of module: ".$mdl);
             $migrationModuleFiles = $this->moduleMigrationFiles($mdl);
+
             if (count($migrationModuleFiles)) {
                 foreach ($migrationModuleFiles as $migration) {
                     if (!in_array($migration['name'], $migrationsRecords)) {
@@ -53,22 +54,24 @@ trait ModuleMigrationTrait
                 }
             } else {
                 $this->components->info('Nothing to migrate.');
-
             }
         }
-
     }
 
     /**
      * @param string|null $moduleName
      * @return array
      */
-    public function moduleMigrationFiles(?string $moduleName = null): array
+    public function moduleMigrationFiles(?string $moduleName = null,): array
     {
         $migrations = [];
 
-        $migrationsPath = module_path($moduleName, 'Database/Migrations');
+        $migrationsPath = module_path($moduleName, config('modules.module.migration_path', 'database/migrations'));
+
+        if (!File::isDirectory($migrationsPath)) return [];
+
         $migration_list = File::allFiles($migrationsPath);
+
         foreach ($migration_list as $key => $migrateFile) {
             $absPath = $migrateFile->getPathname();
             $fileName = $migrateFile->getFilename();
@@ -83,10 +86,10 @@ trait ModuleMigrationTrait
 
     /**
      * @param Collection $migrationTables
-     * @param int $step
+     * @param int        $step
      * @return void
      */
-    public function rollingBackMigration(Collection $migrationTables, int $step = 1): void
+    public function rollingBackMigration(Collection $migrationTables, int $step = 1,): void
     {
         if (empty($migrationTables->pluck('name'))) {
             $this->info("No migrations found for rollback.");
@@ -96,13 +99,13 @@ trait ModuleMigrationTrait
         $records = DB::table('migrations')
                      ->whereIn('migration', $migrationTables->pluck('name'))
                      ->orderBy('batch', 'desc')->get();
+
         if ($records->isEmpty()) {
             $this->info("No matching migrations found in the database.");
             return;
         }
 
         $batchThreshold = $records->max('batch') - $step;
-
 
         $selectingRecord = $records->where('batch', '>', $batchThreshold);
 
@@ -113,7 +116,6 @@ trait ModuleMigrationTrait
                     $class->down();
                 }, $migration['name']);
             }
-
         }
 
         $this->info("updating migrations database.");
@@ -128,17 +130,20 @@ trait ModuleMigrationTrait
 
     /**
      * @return void
+     * @throws \Exception
      */
     public function seedingModules(): void
     {
         $this->warn('seeding module(s)');
 
         $modules = $this->getModules();
+
         foreach ($modules as $module) {
-            $mainSeeder = module_namespace($module);
+            $mainSeeder = moduleNamespace($module)."\\Database\\Seeders\\{$module}DatabaseSeeder";
+
             if (class_exists($mainSeeder)) {
                 $this->runAndCalculate(function () use ($mainSeeder) {
-                    $this->call($mainSeeder);
+                    app($mainSeeder)->__invoke();
                 }, "seeding $module");
             }
         }
